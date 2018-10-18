516bfdd115cc2898ba194e70381570a56b1d4622
xcymo <361855254@qq.com>
2018-10-18 18:34:49 +0800

    upload

--- demo04/src/entry.js
+++ demo04/src/entry.js
@@ -1,3 +1,4 @@
 import css from './css/index.css';
+import less from './css/xcy.less'
 document.write("<h1>大噶好 我系古天乐</h1>");
 document.write("<h1>欢迎来到 贪玩蓝月</h1>");
--- demo04/webpack.config.js
+++ demo04/webpack.config.js
@@ -1,38 +1,62 @@
 const path = require("path");
 const uglify = require("uglifyjs-webpack-plugin");
 const htmlPlugin = require("html-webpack-plugin");
+const extractTextPlugin = require("extract-text-webpack-plugin");
+var myIP = "192.168.43.200"
+var website = {
+    publicPath: "http://" + myIP + ":4567/"
+}
 module.exports = {
     entry: {
         entry: './src/entry.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "[name].js"
+        filename: "[name].js",
+        publicPath: website.publicPath
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
+                use: extractTextPlugin.extract({
+                    fallback: "style-loader",//此项配置是为了预防use中的loader出错而准备的后路，若出错，则使用此项loader
+                    use: "css-loader"
+                })
+            }, {
+                test: /\.(png|jpg|gif|jpeg)/,
                 use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader'
+                    loader: 'url-loader',
+                    options: {
+                        limit: 5000,
+                        outputPath: "images/" //配置生成图片的文件夹
+                    }
                 }]
+            }, {
+                test: /\.(htm|html)$/i,
+                use: ["html-withimg-loader"]
+            }, {
+                test: /\.less$/,
+                use: extractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader', 'less-loader']
+                })
             }
         ]
     },
     plugins: [
-        // new uglify()
+        // new uglify(),
         new htmlPlugin({
             minify: {
                 removeAttributeQuotes: true
             },
             hash: true,
             template: "./src/index.html"
-        })
+        }),
+        new extractTextPlugin("css/xcy.css")  //分离css配置
     ],
     devServer: {
-        host: "192.168.0.112",
+        host: myIP,
         port: 4567,
         compress: true,
         contentBase: path.resolve(__dirname, "dist")
